d87d5829aa3696d516b0649262319a8d5e146572
Anton Medvedev <anton@medv.io>
2019-04-26 23:15:24 +0700

    wip

--- scripts/aviasales.js
+++ scripts/aviasales.js
@@ -3,7 +3,7 @@ const {delay, parsePrice} = require('.')
 module.exports = async ({page, origin, destination, departDate, returnDate}) => {
   await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36')
   // Going to aviasales
-  await page.goto('https://aviasales.ru/search/'+origin+toISOString(departDate)+destination+toISOString(returnDate)+'1')
+  await page.goto('https://aviasales.ru/search/' + origin + toISOString(departDate) + destination + toISOString(returnDate) + '1')
   await delay(3000)
 
   const currencySwitcher = await page.$('span[class="currency-control__label-text"]')
@@ -32,11 +32,11 @@ module.exports = async ({page, origin, destination, departDate, returnDate}) => 
 
 function pad(number) {
   if (number < 10) {
-    return '0' + number;
+    return '0' + number
   }
-  return number;
+  return number.toString()
 }
 
 function toISOString(date) {
-  return (pad(date.getDate()+pad(date.getMonth() + 1)))
+  return (pad(date.getDate()) + pad(date.getMonth() + 1))
 }
