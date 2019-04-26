984595f769c673615f536825649376670bb63d76
Anton Medvedev <anton@medv.io>
2019-04-26 16:11:23 +0700

    wip

--- app/js/subscriptionForm.js
+++ app/js/subscriptionForm.js
@@ -1,10 +1,12 @@
 const {ipcRenderer} = require('electron')
+const $ = require('jquery')
 
 const form = document.querySelector('form')
 form.addEventListener('submit', submitForm)
 
 function submitForm(e) {
   e.preventDefault()
+  console.log($(e.target).serializeArray())
   const origin = document.querySelector('#origin').value
   ipcRenderer.send('add_subscription', origin)
   return true
