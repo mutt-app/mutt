5f7d6159a3468c8d723f3fc0e3b368f6c87b52ee
Anton Medvedev <anton@medv.io>
2019-04-26 23:22:25 +0700

    wip

--- scripts/skyscanner.js
+++ scripts/skyscanner.js
@@ -115,7 +115,7 @@ module.exports = async ({page, origin, destination, departDate, returnDate}) => 
     }
   }
 
-  await delay(10000)
+  await delay(20000)
   const cheapestPriceElement = await page.$('[data-tab="price"] .fqs-price')
   const price = await (await cheapestPriceElement.getProperty('textContent')).jsonValue()
 
